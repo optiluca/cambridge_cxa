8e4dc9ed46613273d8a99987f2fa62807cf6c325
Luca Bortolotti <optiluca@gmail.com>
2025-01-06 15:18:47 +0100

    Cleanup

--- custom_components/cambridge_cxa/media_player.py
+++ custom_components/cambridge_cxa/media_player.py
@@ -1,16 +1,7 @@
-"""
-Support for interface with a Cambridge Audio CXA amplifier over RS-232.
-This is using a remote Raspberry Pi for example that has an USB-to-serial 
-connection to the amplifier.
-
-For more details about this platform, please refer to the documentation at
-https://github.com/lievencoghe/cambridge_cxa
-"""
 import json
 import logging
 import urllib.request
 import paho.mqtt.client as mqtt
-# import serial
 import voluptuous as vol
 
 from homeassistant.components.media_player import MediaPlayerEntity, MediaPlayerEntityFeature, PLATFORM_SCHEMA
@@ -18,9 +9,6 @@ from homeassistant.components.media_player import MediaPlayerEntity, MediaPlayer
 from homeassistant.const import (
     CONF_HOST,
     CONF_NAME,
-    CONF_USERNAME,
-    CONF_PASSWORD,
-    CONF_SLAVE,
     CONF_TYPE,
     STATE_OFF,
     STATE_ON,
@@ -45,11 +33,8 @@ DEFAULT_NAME = "Cambridge Audio CXA"
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_HOST): cv.string,
-        vol.Required(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
         vol.Required(CONF_TYPE): cv.string,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Optional(CONF_SLAVE, default="not set"): cv.string,
     }
 )
 
@@ -128,40 +113,32 @@ AMP_REPLY_MUTE_OFF = "#02,03,0"
 def setup_platform(hass, config, add_devices, discovery_info=None):
     host = config.get(CONF_HOST)
     name = config.get(CONF_NAME)
-    username = config.get(CONF_USERNAME)
-    password = config.get(CONF_PASSWORD)
     cxatype = config.get(CONF_TYPE)
-    cxnhost = config.get(CONF_SLAVE)
 
     if host is None:
-        _LOGGER.error("No SSH relay host found in configuration.yaml for Cambridge CXA")
-        return
-
-    if username is None:
-        _LOGGER.error("No SSH relay username found in configuration.yaml for Cambridge CXA")
+        _LOGGER.error("No MQTT host found in configuration.yaml for Cambridge CXA")
         return
 
     if cxatype is None:
         _LOGGER.error("No CXA type found in configuration.yaml file. Possible values are CXA61, CXA81")
         return
 
-    add_devices([CambridgeCXADevice(host, name, username, password, cxatype, cxnhost)])
+    add_devices([CambridgeCXADevice(host, name, cxatype)])
 
 def on_connect(client, *args, **kwargs):
-    print("on connect: subscribing to base topic...")
+    _LOGGER.info("on connect: subscribing to base topic...")
     client.subscribe('media/cxa81/response')
-    print("on connect: subscribed!")
 
 class CambridgeCXADevice(MediaPlayerEntity):
-    def __init__(self, host, name, username, password, cxatype, cxnhost):
+    def __init__(self, host, name, cxatype):
         _LOGGER.info("Setting up Cambridge CXA")
         self._host = host
 
-        self._mediasource = ""
-        self._speakersactive = ""
+        self._mediasource = "#04,01,16"
+        self._pwstate = AMP_REPLY_PWR_STANDBY
         self._muted = AMP_REPLY_MUTE_OFF
         self._name = name
-        self._pwstate = ""
+        
         self._cxatype = cxatype.upper()
         if self._cxatype == "CXA61":
             self._source_list = NORMAL_INPUTS_CXA61.copy()
@@ -171,9 +148,6 @@ class CambridgeCXADevice(MediaPlayerEntity):
             self._source_reply_list = NORMAL_INPUTS_AMP_REPLY_CXA81.copy()
         self._sound_mode_list = SOUND_MODES.copy()
         self._state = STATE_OFF
-        self._username = username
-        self._password = password
-        self._cxnhost = cxnhost
 
         self.client = mqtt.Client(client_id="ha_cambridge_cxa")
         self.client.on_connect = on_connect
@@ -191,27 +165,23 @@ class CambridgeCXADevice(MediaPlayerEntity):
 
         if AMP_CMD_GET_PWSTATE in msg:
             self._pwstate = msg[AMP_CMD_GET_PWSTATE][0:8]
-            _LOGGER.debug("CXA - update called. State is: %s", self._pwstate)
+            _LOGGER.debug(f"CXA - update called. State is: {self._pwstate}")
         
         if AMP_REPLY_PWR_ON in self._pwstate:
             if AMP_CMD_GET_CURRENT_SOURCE in msg:
                 self._mediasource = msg[AMP_CMD_GET_CURRENT_SOURCE][0:9]
-                _LOGGER.debug("CXA - get current source called. Source is: %s", self._mediasource)
+                _LOGGER.debug(f"CXA - get current source called. Source is: {self._mediasource}")
             
             if AMP_CMD_GET_MUTE_STATE in msg:
                 self._muted = msg[AMP_CMD_GET_MUTE_STATE][0:8]
-                _LOGGER.debug("CXA - current mute state is: %s", self._muted)
-
-
+                _LOGGER.debug(f"CXA - current mute state is: {self._muted}")
+        
+        self.schedule_update_ha_state()
 
     def update(self):
         self._send_mqtt_command_message(AMP_CMD_GET_PWSTATE)
         self._send_mqtt_command_message(AMP_CMD_GET_CURRENT_SOURCE)
         self._send_mqtt_command_message(AMP_CMD_GET_MUTE_STATE)
-
-    def url_command(self, command):
-        _LOGGER.debug("Sending command: %s to: %s", command, self._cxnhost)
-        urllib.request.urlopen("http://" + self._cxnhost + "/" + command).read()
 
     @property
     def is_volume_muted(self):
@@ -219,7 +189,7 @@ class CambridgeCXADevice(MediaPlayerEntity):
             return True
         else:
             return False
-
+    
     @property
     def name(self):
         return self._name
